95e76adc31da561686c798966a67f9846ab8847f
Wesley <113936684+WesleyS08@users.noreply.github.com>
2022-11-27 20:26:49 -0300

    Update tema.js

--- java/tema.js
+++ java/tema.js
@@ -1,9 +1,24 @@
-const themeBtn = document.querySelector('.tema-btn');
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      modeSwitich = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
-themeBtn.addEventListener('click', () => {
-document.body.classList.toggle('dark-theme');
 
-themeBtn.querySelector('span:first-child').classList.toggle('active');
-})
+
+      toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      })
+
+      modeSwitich.addEventListener("click", () => {
+        body.classList.toggle("dark-theme");
+
+        if (body.classList.contains("dark-theme")){
+            modeText.innerText = "Modo claro"
+        }else {
+            modeText.innerText = "Modo Dark"
+
+        }
+      });
 
 
